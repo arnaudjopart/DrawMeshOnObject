95822b1a7bd34b0db4c1fac3d97cfcb2df944561
arnaudjopart <arnaudjopart@users.noreply.github.com>
2021-11-16 22:16:36 +0100

    Adding CircleShape

--- Assets/InputController.cs
+++ Assets/InputController.cs
@@ -36,7 +36,7 @@ public class InputController : MonoBehaviour
 
     private void InitializeDraw()
     {
-        m_shape = new PlaneShape(m_width);
+        m_shape = new DiscShape(10,.2f);
         
         var newObject = new GameObject("mesh");
         var mr = newObject.AddComponent<MeshRenderer>();
@@ -237,22 +237,35 @@ public class DiscShape : MeshShape
 {
     public DiscShape(int _faces ,float _radius)
     {
-        /*m_vertices = new[]
-        {
-            //new Vector2(-_width * .5f, 0),
-            //new Vector2(_width * .5f, 0),
-        };*/
-        m_normals = new[]
-        {
-            Vector2.up,
-            Vector2.up,
-        };
+        m_vertices = new Vector2[_faces];
+        m_normals = new Vector2[_faces];
+        m_us = new int[_faces];
 
-        m_us = new[]
+        m_lines = new int[_faces * 2];
+        
+        var step = 2f / _faces * Mathf.PI;
+        for (var i = 0; i < _faces; i++)
         {
-            0, 0
-        };
-        m_lines = new[] {0, 1};
+            var normalisedPosition = new Vector2(Mathf.Cos(step * i),  Mathf.Sin(step * i));
+            m_vertices[i] = _radius * normalisedPosition;
+            m_normals[i] = normalisedPosition;
+        }
+
+        for (var i = 0; i < _faces; i++)
+        {
+            if (i < _faces - 1)
+            {
+                m_lines[i*2] = i;
+                m_lines[i*2 + 1] = i + 1;
+            }
+            else
+            {
+                m_lines[i*2] = i;
+                m_lines[i*2 + 1] = 0;
+            }
+            
+        }
+        
     }
 }
 
